310457a0f9570255bf135602fa7a311ddb5ad435
Ethan-the-coder <samuellumumba212@gmail.com>
2026-04-07 13:58:05 +0300

    second commit

--- app/src/main/java/com/example/carparkingsystem/ui/theme/screens/dashboard/dashboardScreen.kt
+++ app/src/main/java/com/example/carparkingsystem/ui/theme/screens/dashboard/dashboardScreen.kt
@@ -1,5 +1,7 @@
 package com.example.carparkingsystem.ui.theme.screens.dashboard
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 
@@ -12,12 +14,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
@@ -30,31 +37,48 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.carparkingsystem.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Dashboard(){
+fun Dashboard( onLogoutClick:() -> Unit){
     val selectedItem = remember { mutableStateOf(0) }
+
+
     Scaffold(
         topBar = {
             TopAppBar(
-                title = {Text(text = "Parking Dashboard")},
+                title = {Text(text = "Parking Dashboard",
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold)
+                        },
+                actions = { IconButton(onClick = onLogoutClick){
+                    Icon(
+                        imageVector = Icons.Default.ExitToApp,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.error
+                    )
+                } },
+
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color.Gray,
+                    containerColor = Color(0xFFF7FB),
                     titleContentColor = Color.Black,
 
                 )
+
             )
         },
 
 
         bottomBar = {
             NavigationBar(
-                containerColor = Color.Gray
+                containerColor = Color(0xFFF7FB)
             )  {
                 NavigationBarItem(
                     selected = selectedItem.value == 0,
@@ -82,12 +106,14 @@ fun Dashboard(){
 
     )
 
+
+
     { padding ->
         Column(modifier = Modifier.padding(padding))
         {
             Text(
                 text = "Smart Parking System",
-                color = Color.Green,
+                color = Color.Black,
                 fontSize = 30.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(bottom = 20.dp),
@@ -96,15 +122,22 @@ fun Dashboard(){
             Card(
                 modifier = Modifier.fillMaxWidth().padding(bottom = 20.dp),
                 shape = RoundedCornerShape(20.dp),
-                colors = CardDefaults.cardColors( containerColor = Color.Green)
+                colors = CardDefaults.cardColors( containerColor = Color(0xFFE8F5E9))
             ) {
                 Row(
                     modifier = Modifier.fillMaxWidth().padding(20.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+
                 ) {
                     Column {
-                        Text(text = "Available", color = Color.Black)
-                        Text(text = "18 Slots", color = Color.Black, fontSize = 20.sp)
+                        Text(text = "Available", color = Color(0xFFE8F5E9),
+                            style = MaterialTheme.typography.labelLarge)
+                        Text(text = "18 Slots", color = Color(0xFF2E7D32), fontSize = 28.sp,
+                            fontWeight = FontWeight.ExtraBold
+                        )
+
+
                     }
                 }
             }
@@ -112,10 +145,27 @@ fun Dashboard(){
                 modifier = Modifier.fillMaxWidth().padding(10.dp),
                 horizontalArrangement = Arrangement.spacedBy(12.dp)
             ) {
+
+//                ActionCard(
+//                    title = "Add Car",
+//                    icon = Icons.Default.AddCircle,
+//                    backgroundColor = Color(0xFF6CABDD), // Sky Blue
+//                    modifier = Modifier.weight(1f),
+//                    onClick = { /* navigate to add */ }
+//                )
+//                ActionCard(
+//                    title = "View Cars",
+//                    icon = Icons.Default.DirectionsCar,
+//                    backgroundColor = Color(0xFF5F6368), // Professional Gray
+//                    modifier = Modifier.weight(1f),
+//                    onClick = { /* navigate to list */ }
+//                )
+
                 Card(
-                    modifier = Modifier.size(100.dp),
+                    modifier = Modifier.size(90.dp),
                     shape = RoundedCornerShape(20.dp),
-                    colors = CardDefaults.cardColors( containerColor = Color.DarkGray)
+                    colors = CardDefaults.cardColors( containerColor = Color.Gray)
+
                 ) {
                     Box(
                         modifier = Modifier.fillMaxSize(),
@@ -126,9 +176,9 @@ fun Dashboard(){
                     
                 }
                 Card(
-                    modifier = Modifier.size(100.dp),
+                    modifier = Modifier.size(90.dp),
                     shape = RoundedCornerShape(20.dp),
-                    colors = CardDefaults.cardColors( containerColor = Color.DarkGray)
+                    colors = CardDefaults.cardColors( containerColor = Color.Gray)
 
                 ) {
                     Box(
@@ -153,8 +203,12 @@ fun Dashboard(){
 }
 
 
+
 @Preview( showBackground = true, showSystemUi = true)
 @Composable
+
+
+
 fun DashboardScreenPreview(){
-    Dashboard()
+    Dashboard(onLogoutClick = {})
 }
